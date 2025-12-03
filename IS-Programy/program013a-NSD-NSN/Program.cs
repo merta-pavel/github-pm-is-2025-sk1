670544c060ea214fb7fc75fd7f8055bbbc3e95c2
Pavel Merta <pavel.merta05@gmail.com>
2025-12-03 10:10:36 +0100

    #15 Základní kód

---
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -0,0 +1,45 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Název programu *******");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Pavel Merta ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu - řešený správně
+    Console.Write("Zadejte hodnotu (celé číslo): ");
+    int a;
+
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+
+    Console.Write("Zadejte hodnotu (celé číslo): ");
+    int b;
+
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+    }
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+
+}
+
+
+
